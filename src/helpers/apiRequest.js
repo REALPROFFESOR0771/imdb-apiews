f866f0e6163e6fe03c52707655d707108eeae4b6
PROFFESOR <157197613+REALPROFFESOR0771@users.noreply.github.com>
2025-11-19 13:52:20 +0530

    Refactor apiRequest functions with updated headers

--- src/helpers/apiRequest.js
+++ src/helpers/apiRequest.js
@@ -1,21 +1,31 @@
 export default async function apiRequestRawHtml(url) {
-    const res = await fetch(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        accept: "text/html",
-        "accept-language": "en-US"
-      }
-    });
-    return await res.text();
-  }
-  
-  export async function apiRequestJson(url) {
-    const res = await fetch(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-        accept: "application/json",
-        "accept-language": "en-US"
-      }
-    });
-    return await res.json();
-  }
+  const res = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-User": "?1",
+      "Sec-Fetch-Dest": "document",
+      "Upgrade-Insecure-Requests": "1"
+    }
+  });
+
+  return await res.text();
+}
+
+export async function apiRequestJson(url) {
+  const res = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Accept": "application/json",
+      "Accept-Language": "en-US,en;q=0.9"
+    }
+  });
+
+  return await res.json();
+}
